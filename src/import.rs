7c687e0ef934c4f5d737b629f9cc379296a9b0f5
Katavga <meucci.ilunga@gmail.com>
2022-04-19 00:23:50 -0700

    Rewrote exisiting implementation of import module; dropped support for parsing in assembly_summary.txt files. Instead, primary assembly metadata will be taken from full database assembly summary files (i.e., assembly_summary_refseq.txt or genbank equivalent; these are the same files that were used in my python scripts to determine which files to download from the NCBI ftp server). In addition to building a new metadata importer based on the general database summary files, I also implemented the *_genomic.fna (i.e., assembly genome fasta) importer. As opposed to my proof-of-concept project, this import parser makes much better use of Rust's iterators, and its method is more robust than the previous project's; essentially, the program uses the definition lines containing the '>' character to define which lines of imported text belong to replicon/genomic element. After determining which lines belong to which replicon, the program merges them, and then parses the definition line to pull out a genomic accession identifier meant to serve as the name of the parsed genome sequence.

--- src/import.rs
+++ src/import.rs
@@ -1,224 +1,234 @@
 #![allow(dead_code)]
-#![allow(unused_imports)]
-
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
-use std::path::{Path, PathBuf};
-use std::error::Error;
+use std::path::PathBuf;
 
-fn parse_assembly_report(assembly_report_path: PathBuf) -> Result<AssemblyReportMetadata, Box<dyn Error>> {
-    let mut file = File::open(assembly_report_path)?;
+fn import_database_summary(summary_file_path: PathBuf) -> HashMap<String, AssemblyMetadata> {
+
+    // Storage logistics
+    let mut database_metadata_table: HashMap<String, AssemblyMetadata> = HashMap::new(); 
+
+    // Read-in Metadata file
+    let mut file = File::open(summary_file_path).expect("ERROR: could not open metadata summary file!");
     let mut contents = String::new();
-
-    file.read_to_string(&mut contents)?;
+    file.read_to_string(&mut contents).expect("ERROR: could not read string data from metadata summary file!");
+    
+    // Parse lines from Metadata file
     let metadata_content = contents.split('\n');
+    let mut metadata_entries: Vec<String> = vec![];
+    for (index, item) in metadata_content.enumerate() {
 
-    // Pull target metadata entries
-    let mut metadata_tmp: Vec<String> = Vec::with_capacity(6);
-    let mut replicon_tmp: Vec<AssemblyReportRepliconMetadata> = Vec::new();
-    let mut replicon_flag: bool = false;
-    let metadata_tags = [
-        "# Assembly name:  ",
-        "# Organism name:  ",
-        "# Taxid:          ",
-        "# Assembly level: ",
-        "# GenBank assembly accession: ",
-        "# RefSeq assembly accession: ",
-    ];
-
-    // Interrogate file line-by-line
-    for line in metadata_content {
-
-        // ignore lines that are blank
-        if line.len() == 0 {
+        // Skip first line of metadata file!
+        if index == 0 {
             continue;
         }
 
-        // Parse top-level metadata
-        if !replicon_flag { // if replicon flag hasn't been triggered
-            for tag in metadata_tags {
-                if line.contains(tag) {
-                    let mut new_line = line.split(tag).collect::<Vec<&str>>()[1].to_string();
-                    new_line.pop().unwrap(); // remove newline character
-                    metadata_tmp.push(new_line.to_string());
-                    break;
-                }
-            }
-        }
-
-        // Anything after this line would correspond to individual replicon metadata
-        if line.contains("# Sequence-Name	Sequence-Role",) {
-            replicon_flag = true;
-            continue;
-        }
-
-        // Parse individual replicon metadata
-        if replicon_flag {
-            let cleaned_line = line.strip_suffix('\n');
-            let new_line = match cleaned_line {
-                None => line,
-                Some(x) => x,
-            };
-
-            let replicon_parse: Vec<&str> = new_line.split('\t').collect();
-            
-            let replicon_meta = AssemblyReportRepliconMetadata {
-                seq_name: replicon_parse[0].to_string(),
-                location: replicon_parse[3].to_string(),
-                genbank_accession: replicon_parse[4].to_string(),
-                refseq_accession: replicon_parse[6].to_string(),
-                sequence_len: str::parse(replicon_parse[8]).unwrap(),
-            };
-
-            replicon_tmp.push(replicon_meta);
-        }
+        // Store entries
+        metadata_entries.push(item.to_string());
     }
 
-    // Consolidate parsed data
-    let output = AssemblyReportMetadata {
-        assembly_name: metadata_tmp[0].clone(),
-        organism_name: metadata_tmp[1].clone(),
-        taxid: str::parse(&metadata_tmp[2]).unwrap(),
-        assembly_level: metadata_tmp[3].clone(),
-        genbank_accesion: metadata_tmp[4].clone(),
-        refseq_accesion: metadata_tmp[5].clone(),
-        replicon_metadata: replicon_tmp,
-    };
+    // Filter off last entry, since it is just an empty string; aka, remove parsing artifact
+    let modified_metadata_entries = metadata_entries.into_iter()
+                                                    .filter(|s| !s.eq(&"".to_string()))
+                                                    .collect::<Vec<String>>();
 
-    Ok(output)
+    // Parse individual data entries from file
+    for line in modified_metadata_entries {
+        let entries: Vec<String> = line.split('\t')
+                                       .map(|s| s.chars().collect::<String>())
+                                       .collect();
+
+        let assembly_entry = AssemblyMetadata::new(entries);
+        database_metadata_table.insert(assembly_entry.subdir_name.clone(), assembly_entry);
+    }
+    
+    // Map assembly metadata (val) against assembly directory name (key)
+    database_metadata_table
 }
 
-fn parse_genome_sequence() {}
+fn parse_genome_sequence(genome_name: String, genome_fasta_file: PathBuf) -> GenomeSequence {
+    
+    // Read-in Metadata file
+    let mut file = File::open(genome_fasta_file).expect("ERROR: could not open metadata summary file!");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("ERROR: could not read string data from metadata summary file!");
 
+    // Parse fasta data into individual files
+    let lines_of_sequence = contents.split('\n');
+    let mut parsed_sequence: Vec<String> = vec![];
+    let mut definition_line_indicies: Vec<usize> = vec![];
+    let mut sequence_bounds: Vec<(usize, usize)> = vec![];
+    let mut replicon_data: Vec<RepliconSequence> = vec![];
 
-#[derive(PartialEq, Debug)]
-struct AssemblyReportMetadata {
-    taxid: usize,
-    assembly_name: String,
-    organism_name: String,
-    assembly_level: String,
-    genbank_accesion: String,
-    refseq_accesion: String,
-    replicon_metadata: Vec<AssemblyReportRepliconMetadata>,
+    // Determine replicon sequence bounds by determining location of 
+    // every definition line by looking for '>' character
+    for (index, line) in lines_of_sequence.enumerate() {
+
+        // Final bound is defined by line with no characters at the end of every file
+        if line.contains('>') || line.len() == 0 {
+            definition_line_indicies.push(index);
+        }
+        parsed_sequence.push(line.to_string());
+    }
+
+    // Define the replicon sequence boundaries
+    let start_indicies = definition_line_indicies.iter();
+    let end_indicies = definition_line_indicies[1..].iter();
+    for (&start, &end) in start_indicies.zip(end_indicies) {
+        let new_bound = (start+1, end);
+        sequence_bounds.push(new_bound);
+    }
+
+    // Parse out individual subsequences from FASTA data via bound indicies
+    for (start, end) in sequence_bounds {
+        let replicon_sequence = (&parsed_sequence[start..end].join("")).clone();
+
+        // Parse out genomic accession identifier from definition
+        let replicon_accession = parsed_sequence[start-1].clone()
+                                                         .split(' ')
+                                                         .map(|s| s.to_string())
+                                                         .collect::<Vec<String>>()[0]
+                                                         .clone()
+                                                         .chars()
+                                                         .filter(|&c| c != '>')
+                                                         .collect::<String>();
+
+        let new_replicon = RepliconSequence {
+            replicon_accession,
+            replicon_sequence,
+        };
+
+        replicon_data.push(new_replicon);
+    }
+
+    // Wrap data into a genome sequence
+    GenomeSequence {
+        genome_name,
+        genomic_elements: replicon_data
+    }
 }
 
-#[derive(PartialEq, Debug)]
-struct AssemblyReportRepliconMetadata {
-    seq_name: String,
-    location: String,
-    genbank_accession: String,
-    refseq_accession: String,
-    sequence_len: usize,
+#[derive(PartialEq, Debug, Clone)]
+struct AssemblyMetadata {
+    assembly_accession:     String,
+    biosample_accession:    String,
+    ref_seq_cat:            String,
+    tax_id:                 String,
+    species_tax_id:         String,
+    organism_name:          String,
+    infraspecifc_name:      String,
+    assembly_level:         String,
+    assembly_name:          String,
+    ftp_location:           String,
+    subdir_name:            String,
+    ref_seq_status:         String,
+}
+
+impl AssemblyMetadata {
+    fn new(entry_data: Vec<String>) -> AssemblyMetadata {
+
+        let assembly_accession = entry_data[0].to_string();
+        let assembly_name = entry_data[15].to_string();
+        let subdir_name = format!("{}_{}", assembly_accession, assembly_name);
+
+        AssemblyMetadata {
+            assembly_accession,
+            biosample_accession:    entry_data[2].to_string(),
+            ref_seq_cat:            entry_data[4].to_string(),
+            tax_id:                 entry_data[5].to_string(),
+            species_tax_id:         entry_data[6].to_string(),
+            organism_name:          entry_data[7].to_string(),
+            infraspecifc_name:      entry_data[8].to_string(),
+            assembly_level:         entry_data[11].to_string().to_lowercase(),
+            assembly_name,
+            ftp_location:           entry_data[19].to_string(),
+            subdir_name,
+            ref_seq_status:         entry_data[20].to_string()
+        }
+    }
+}
+
+struct GenomeSequence {
+    genome_name: String,
+    genomic_elements: Vec<RepliconSequence>
+}
+
+#[derive(PartialEq, Debug, Clone)]
+struct RepliconSequence {
+    replicon_accession: String,
+    replicon_sequence: String,
 }
 
 struct GeneralAnnotationData {}
 
 struct CopYAnnoationData {}
 
-struct GenomeSequenceData {}
-
 #[cfg(test)]
 mod tests {
     use std::path::PathBuf;
     use super::*;
-    
+
     #[test]
-    fn assembly_report_import_test_1() {
-        
-        // Import test asset
-        let test_file = "test_assets/GCA_000152665.1_ASM15266v1/GCA_000152665.1_ASM15266v1_assembly_report.txt";
+    fn test_database_metadata_import_1() {
+        let test_file = "database_management/data_assets/assembly_summary_refseq.txt";
         let test_file = PathBuf::from(test_file);
-        
-        // Define correct answer manually
-        let replicon_meta = AssemblyReportRepliconMetadata {
-            seq_name: "ANONYMOUS".to_string(),
-            location: "Chromosome".to_string(),
-            genbank_accession: "CM000287.4".to_string(),
-            refseq_accession: "NZ_CM000287.1".to_string(),
-            sequence_len: 4109689_usize,
-        };
-        
-        let expected_assembly_meta = AssemblyReportMetadata {
-            taxid: 367459_usize,
-            assembly_name: "ASM15266v1".to_string(),
-            organism_name: "Clostridioides difficile QCD-32g58 (firmicutes)".to_string(),
-            assembly_level: "Chromosome".to_string(),
-            genbank_accesion: "GCA_000152665.1".to_string(),
-            refseq_accesion: "GCF_000152665.1".to_string(),
-            replicon_metadata: vec![replicon_meta],
+        let database_metadata = import_database_summary(test_file);
+
+        let expected_answer = AssemblyMetadata { 
+            assembly_accession:     "GCF_001735525.1".to_string(), 
+            biosample_accession:    "SAMN05384437".to_string(), 
+            ref_seq_cat:            "representative genome".to_string(), 
+            tax_id:                 "23".to_string(), 
+            species_tax_id:         "23".to_string(),
+            organism_name:          "Shewanella colwelliana".to_string(),
+            infraspecifc_name:      "strain=CSB03KR".to_string(),
+            assembly_level:         "scaffold".to_string(),
+            assembly_name:          "ASM173552v1".to_string(),
+            ftp_location:           "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/001/735/525/GCF_001735525.1_ASM173552v1".to_string(),
+            subdir_name:            "GCF_001735525.1_ASM173552v1".to_string(),
+            ref_seq_status:         "".to_string()
         };
 
-        let actual_assembly_meta = parse_assembly_report(test_file).unwrap();
-        assert_eq!(expected_assembly_meta, actual_assembly_meta);
+        let test_key = "GCF_001735525.1_ASM173552v1".to_string();
+        let actual_answer = (*database_metadata.get(&test_key).unwrap()).clone();
+        println!("{:?}", expected_answer);
+
+        assert_eq!(expected_answer, actual_answer)
     }
 
     #[test]
-    fn assembly_report_import_test_2() {
-        
-        // Import test asset
-        let test_file = "test_assets/GCF_000009725.1_ASM972v1/GCF_000009725.1_ASM972v1_assembly_report.txt";
+    fn test_database_metadata_import_2() {
+        let test_file = "database_management/data_assets/assembly_summary_genbank.txt";
         let test_file = PathBuf::from(test_file);
-        
-        // Define correct answer manually
-        let replicon_meta_1 = AssemblyReportRepliconMetadata {
-            seq_name: "ANONYMOUS".to_string(),
-            location: "Chromosome".to_string(),
-            genbank_accession: "BA000022.2".to_string(),
-            refseq_accession: "NC_000911.1".to_string(),
-            sequence_len: 3573470_usize,
+        let database_metadata = import_database_summary(test_file);
+
+        let expected_answer = AssemblyMetadata { 
+            assembly_accession:     "GCA_001465115.1".to_string(), 
+            biosample_accession:    "SAMN04296138".to_string(), 
+            ref_seq_cat:            "na".to_string(), 
+            tax_id:                 "332949".to_string(), 
+            species_tax_id:         "332949".to_string(),
+            organism_name:          "Enterococcus silesiacus".to_string(),
+            infraspecifc_name:      "strain=LMG 23085".to_string(),
+            assembly_level:         "complete genome".to_string(),
+            assembly_name:          "ASM146511v1".to_string(),
+            ftp_location:           "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/001/465/115/GCA_001465115.1_ASM146511v1".to_string(),
+            subdir_name:            "GCA_001465115.1_ASM146511v1".to_string(),
+            ref_seq_status:         "missing tRNA genes".to_string()
         };
 
-        let replicon_meta_2 = AssemblyReportRepliconMetadata {
-            seq_name: "pSYSA".to_string(),
-            location: "Plasmid".to_string(),
-            genbank_accession: "AP004311.1".to_string(),
-            refseq_accession: "NC_005230.1".to_string(),
-            sequence_len: 103307_usize,
-        };
+        let test_key = "GCA_001465115.1_ASM146511v1".to_string();
+        let actual_answer = (*database_metadata.get(&test_key).unwrap()).clone();
+        println!("{:?}", expected_answer);
 
-        let replicon_meta_3 = AssemblyReportRepliconMetadata {
-            seq_name: "pSYSG".to_string(),
-            location: "Plasmid".to_string(),
-            genbank_accession: "AP004312.1".to_string(),
-            refseq_accession: "NC_005231.1".to_string(),
-            sequence_len: 44343_usize,
-        };
+        assert_eq!(expected_answer, actual_answer)
+    }
 
-        let replicon_meta_4 = AssemblyReportRepliconMetadata {
-            seq_name: "pSYSM".to_string(),
-            location: "Plasmid".to_string(),
-            genbank_accession: "AP004310.1".to_string(),
-            refseq_accession: "NC_005229.1".to_string(),
-            sequence_len: 119895_usize,
-        };
-
-        let replicon_meta_5 = AssemblyReportRepliconMetadata {
-            seq_name: "pSYSX".to_string(),
-            location: "Plasmid".to_string(),
-            genbank_accession: "AP006585.1".to_string(),
-            refseq_accession: "NC_005232.1".to_string(),
-            sequence_len: 106004_usize,
-        };
-        
-        let expected_assembly_meta = AssemblyReportMetadata {
-            taxid: 1148,
-            assembly_name: "ASM972v1".to_string(),
-            organism_name: "Synechocystis sp. PCC 6803 (cyanobacteria)".to_string(),
-            assembly_level: "Complete Genome".to_string(),
-            genbank_accesion: "GCA_000009725.1".to_string(),
-            refseq_accesion: "GCF_000009725.1".to_string(),
-            replicon_metadata: vec![
-                replicon_meta_1,
-                replicon_meta_2,
-                replicon_meta_3,
-                replicon_meta_4,
-                replicon_meta_5,
-
-            ],
-        };
-
-        let actual_assembly_meta = parse_assembly_report(test_file).unwrap();
-        assert_eq!(expected_assembly_meta, actual_assembly_meta);
+    #[test]
+    fn parse_genome_sequence_1() {
+        let test_file = "test_assets/GCF_000009725.1_ASM972v1/GCF_000009725.1_ASM972v1_genomic.fna";
+        let test_file = PathBuf::from(test_file);
+        let genome = parse_genome_sequence("GCF_000009725.1_ASM972v1".to_string(), test_file);
     }
 }
